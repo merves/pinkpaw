ef6c23eda84bce2be522b50f65c045509438b004
merves <merves@users.noreply.github.com>
2018-11-17 19:07:58 +0300

    Create ThreatmentActivity.java

---
+++ PetMobile/app/src/main/java/com/example/trk/petmobile/ThreatmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.trk.petmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreatmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_threatment);
+    }
+}
